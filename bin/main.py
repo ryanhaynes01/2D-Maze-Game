8c2759b5d52b322c7b4ab97ceb781448de6a034f
ryanhaynes01 <50088158+ryanhaynes01@users.noreply.github.com>
2019-05-26 23:32:30 +0100

    Comments added

--- bin/main.py
+++ bin/main.py
@@ -10,7 +10,7 @@ class windowSetup:
         self.height = HEIGHT
         self.caption = "2D Maze Game"
 
-    def create(self): # game screen creation for rest of the game
+    def create(self):                                                    # game screen creation for game
         self.window = pygame.display.set_mode((self.width, self.height))
         pygame.display.set_caption(self.caption)
 
@@ -22,18 +22,18 @@ class eventHandler:
 
     def gameEdge(self):
         gameScreen = self.display
-        pygame.draw.rect(gameScreen, (211,211,211), (0, 0, WIDTH, 10))
-        pygame.draw.rect(gameScreen, (211,211,211), (0, 0, 10, HEIGHT))
-        pygame.draw.rect(gameScreen, (211,211,211), (WIDTH-10, 0, WIDTH, HEIGHT))
-        pygame.draw.rect(gameScreen, (211,211,211), (0, HEIGHT-10, WIDTH, HEIGHT))
-        pygame.display.update()
+        pygame.draw.rect(gameScreen, (211,211,211), (0, 0, WIDTH, 10))              # top rectangle
+        pygame.draw.rect(gameScreen, (211,211,211), (0, 0, 10, HEIGHT))             # left rectangle
+        pygame.draw.rect(gameScreen, (211,211,211), (WIDTH-10, 0, WIDTH, HEIGHT))   # right rectangle
+        pygame.draw.rect(gameScreen, (211,211,211), (0, HEIGHT-10, WIDTH, HEIGHT))  # bottom rectangle
+        pygame.display.update()                                                     # draws rectangles to screen
         self.display = gameScreen
 
     def events(self):
         self.gameEdge()
         clock = pygame.time.Clock()           # clock is declared for fps limit
         while self.gameState:
-            clock.tick(self.FPS)
+            clock.tick(self.FPS)              # fps limit set
             for event in pygame.event.get():  # fetches all events from pygame
                 if event.type == pygame.QUIT: # if the x is pressed, close
                     self.gameState = False    # break the event loop
@@ -41,12 +41,12 @@ class eventHandler:
 
 def main():
     game = True
-    displayCreation = windowSetup()
-    displayCreation.create()
-    eventUpdates = eventHandler(displayCreation.window, game)
+    displayCreation = windowSetup()                           # initialises windowSetup
+    displayCreation.create()                                  # window is made
+    eventUpdates = eventHandler(displayCreation.window, game) # initialises eventHandler
     while game:
-        eventUpdates.events()
-        game = eventUpdates.gameState
+        eventUpdates.events()                                 # interates events 
+        game = eventUpdates.gameState                         # makes gamestate global
     pygame.quit()
     pass
 

fcbbda43368150da77aa48a63a742462bf403e02
ryanhaynes01 <50088158+ryanhaynes01@users.noreply.github.com>
2019-05-29 15:46:31 +0100

    Added a sprite to bin and added player movement
    Player movement and borders.

--- main.py
+++ main.py
@@ -14,15 +14,31 @@ class windowSetup:
         pygame.display.set_caption(self.caption)
 
 class playerHandler:
-    def __init__(self):
-        self.initX = 0
-        self.initY = 0
+    def __init__(self, gameScreen):
+        self.initX = 10
+        self.initY = 10
+        self.display = gameScreen
+        self.idleAnimation = pygame.image.load("bin/sprites/adventurer-idle-00.png") # loads player sprite from bin
 
+    def playerDraw(self):
+        self.display.blit(self.idleAnimation, (self.initX, self.initY))
+
+    def playerMovement(self, pressedKeys):
+        if pressedKeys[pygame.K_RIGHT] and self.initX < WIDTH - 5 - 50: # moves player right if in boundary
+            self.initX += 4
+        if pressedKeys[pygame.K_LEFT] and self.initX > 5:               # moves player left if in boundary
+            self.initX -= 4
+        if pressedKeys[pygame.K_UP] and self.initY > 5:                 # moves player up if in boundary
+            self.initY -= 4
+        if pressedKeys[pygame.K_DOWN] and self.initY < HEIGHT - 5 - 37: # moves player down if in boundary
+            self.initY += 4
+        
 class eventHandler:
     def __init__(self, gameScreen, game):
         self.FPS = 60
         self.display = gameScreen
         self.gameState = game
+        self.player = playerHandler(self.display)
 
     def gameEdge(self):
         gameScreen = self.display
@@ -31,18 +47,25 @@ class eventHandler:
         pygame.draw.rect(gameScreen, (211,211,211), (0, 0, edgeSize, HEIGHT))            # left rectangle
         pygame.draw.rect(gameScreen, (211,211,211), (WIDTH-edgeSize, 0, WIDTH, HEIGHT))  # right rectangle
         pygame.draw.rect(gameScreen, (211,211,211), (0, HEIGHT-edgeSize, WIDTH, HEIGHT)) # bottom rectangle
-        pygame.display.update()                                                          # draws rectangles to screen
         self.display = gameScreen
 
+    def draw(self):
+        pygame.display.update() # draws everything blit to screen at once
+
     def events(self):
-        self.gameEdge()
         clock = pygame.time.Clock()           # clock is declared for fps limit
         while self.gameState:
+            self.display.fill((0, 0, 0))      # refreshes background
+            self.player.playerDraw()          # player blit update
+            self.gameEdge()                   # keeps screen edge updated
+            self.draw()                       # update screen for all blits
             clock.tick(self.FPS)              # fps limit set
             for event in pygame.event.get():  # fetches all events from pygame
                 if event.type == pygame.QUIT: # if the x is pressed, close
                     self.gameState = False    # break the event loop
                 print(event)                  # show events for debug
+            keys = pygame.key.get_pressed()
+            self.player.playerMovement(keys)
 
 def main():
     game = True
